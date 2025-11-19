1c8f13e392acd7818136d9bcc9ee2804cc98278d
Matt McKenna <matthew.w.mckenna@spiritaero.com>
2025-11-19 12:45:57 -0600

    Added a timeout for receiving packets
    - `recvfrom` blocks indefinitely when waiting for the first packet.
      Then timeout is used for every subsequent packet (currently 5s).
    - Added simple CLI for providing IP address and output file path

--- src/main.c
+++ src/main.c
@@ -1,12 +1,11 @@
+#include <errno.h>
 #include <stdbool.h>
+#include <sys/time.h>
 
 #define CIMPL_IMPLEMENTATION
 #include "cimpl_core.h"
 #include "cimpl_glm.h"
 #include "cimpl_network.h"
-
-#define SERVER_IP_ADDR "127.0.0.1:6969"
-#define OUTPUT_FILE "UCGAC2413007_poses_60_64.csv"
 
 typedef struct Pose {
     char id[32];
@@ -17,47 +16,135 @@ typedef struct Pose {
     bool trigger_activated;
 } Pose;
 
-int main(void) {
+i32 udp_listener_setup_with_timeout(
+    isize* socket_fd, IpV4Addr ip, uint32_t timeout_sec
+) {
+    struct sockaddr_in listen_addr = {0};
+    listen_addr.sin_family = AF_INET;
+    listen_addr.sin_addr.s_addr = htonl(ip.addr);
+    listen_addr.sin_port = htons(ip.port);
+
+    char ip_addr_str[INET_ADDRSTRLEN] = {0};
+    if (inet_ntop(
+            listen_addr.sin_family,
+            &(listen_addr.sin_addr),
+            ip_addr_str,
+            INET_ADDRSTRLEN
+        ) == NULL) {
+        log_error("Malformed IP address.\n");
+        return EXIT_FAILURE;
+    }
+
+    *socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (*socket_fd < 0) {
+        log_error("Failed to create socket for %s:%d.", ip_addr_str, ip.port);
+        return EXIT_FAILURE;
+    }
+
+    int reuse = 1;
+    if (setsockopt(
+            *socket_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)
+        ) < 0) {
+        log_error("Failed to set SO_REUSEADDR");
+    }
+
+    struct timeval timeout_tv = {
+        .tv_sec = timeout_sec,
+    };
+    if (setsockopt(
+            *socket_fd, SOL_SOCKET, SO_RCVTIMEO, &timeout_tv, sizeof(timeout_tv)
+        ) < 0) {
+        log_error("Failed to set SO_RCVTIMEO");
+    }
+
+    int bind_err = bind(
+        *socket_fd, (const struct sockaddr*)&listen_addr, sizeof(listen_addr)
+    );
+    if (bind_err < 0) {
+        log_error(
+            "[%d] Failed to bind to %s:%d.", bind_err, ip_addr_str, ip.port
+        );
+        return EXIT_FAILURE;
+    }
+    log_info("Listening at %s:%d\n", ip_addr_str, ip.port);
+    return EXIT_SUCCESS;
+}
+
+void help(char** argv) {
+    printf("Usage: %s [IP_ADDR] [OUTPUT_PATH]", argv[0]);
+    return;
+}
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        help(argv);
+        return -1;
+    }
     IpV4Addr server_addr = {0};
-    IpV4Addr_from_str(&server_addr, SERVER_IP_ADDR);
+    IpV4Addr_from_str(&server_addr, argv[1]);
     isize socket_fd = -1;
-    udp_listener_setup(&socket_fd, server_addr);
+
+    udp_listener_setup_with_timeout(&socket_fd, server_addr, 5);
 
     struct sockaddr_in client_info = {0};
     socklen_t client_len = sizeof(client_info);
 
-    FILE* fp = fopen(OUTPUT_FILE, "w");
+    FILE* fp = fopen(argv[2], "w");
     if (fp == NULL) {
         perror("fopen");
         return 1;
     }
     fprintf(fp, "id,px,py,pz,qx,qy,qz,qw\n");
     Pose recv_pose = {0};
-    while (1) {
-        isize recv_bytes = recvfrom(
-            socket_fd,
-            &recv_pose,
-            sizeof(Pose),
-            0,
-            (struct sockaddr*)&client_info,
-            &client_len
-        );
+    u32 pose_count = 0;
+    while (pose_count < 165) {
+        // Don't use timeout until the first message has been received
+        isize recv_bytes;
+        if (pose_count == 0) {
+            recv_bytes = recvfrom(
+                socket_fd,
+                &recv_pose,
+                sizeof(Pose),
+                0,
+                (struct sockaddr*)&client_info,
+                &client_len
+            );
+        } else {
+            recv_bytes = recvfrom(
+                socket_fd,
+                &recv_pose,
+                sizeof(Pose),
+                0,
+                (struct sockaddr*)&client_info,
+                &client_len
+            );
+        }
 
         if (recv_bytes == -1) {
-            fprintf(stderr, "Failed to receive packet\n");
-            perror("recvfrom");
-            close(socket_fd);
-            fclose(fp);
-            return 1;
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                if (pose_count > 0) {
+                    fprintf(
+                        stderr, "Timout reached trying to receive packet.\n"
+                    );
+                    break;
+                }
+                continue;
+            } else {
+                fprintf(stderr, "Failed to receive packet\n");
+                perror("recvfrom");
+                break;
+            }
         }
 
         printf("Bytes received: %ld\n", recv_bytes);
         fprintf(
             fp,
             "%s"
+            ",%d"
             ",%12.6f,%12.6f,%12.6f"
             ",%12.6f,%12.6f,%12.6f,%12.6f\n",
             recv_pose.id,
+            pose_count,
             recv_pose.position.x,
             recv_pose.position.y,
             recv_pose.position.z,
@@ -66,8 +153,10 @@ int main(void) {
             recv_pose.rotation.z,
             recv_pose.rotation.w
         );
+        pose_count++;
     }
 
     close(socket_fd);
     fclose(fp);
+    return 0;
 }
